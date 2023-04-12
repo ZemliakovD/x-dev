bad918cf56947753acb8c0a052f381b9d69b2645
Dmitri Zemliakov <112906798+ZemliakovD@users.noreply.github.com>
2023-04-12 17:02:52 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,74 @@
-$(function(){
+  $('.partfolio-slider').slick({
+    prevArrow: '<button type="button" class="prev-button"><i class="fa-solid fa-angle-left fa-beat" style="color: #2EECC5;"></i></button>',
+    nextArrow: '<button type="button" class="next-button"><i class="fa-solid fa-angle-right fa-beat" style="color: #2EECC5;"></i></button>',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 
-});
+  $('.rates-slider').slick({
+    prevArrow: '<button type="button" class="prev-button"><i class="fa-solid fa-angle-left fa-beat" style="color: #2EECC5;"></i></button>',
+    nextArrow: '<button type="button" class="next-button"><i class="fa-solid fa-angle-right fa-beat" style="color: #2EECC5;"></i></button>',
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $("#main").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+    scrollTop: $(".main-content").offset().top // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+   });
+
+   $("#reasons").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+    scrollTop: $(".reasons-inner").offset().top // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+   });
+
+   $("#contacts").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+    scrollTop: $(".footer").offset().top // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+   });
